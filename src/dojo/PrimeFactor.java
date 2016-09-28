42820b7b1cc88561368d58487ebd00402dd94ceb
JackyShen <mebusw@163.com>
2016-09-28 22:56:12 +0800

    add recursive solution of PrimeFactor

--- src/dojo/PrimeFactor.java
+++ src/dojo/PrimeFactor.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Created by jacky on 14-11-12.
  */
 public class PrimeFactor {
-    public static List factors(int i) {
+    public static List factors1(int i) {
         List result = new ArrayList();
         for (int factor = 2; factor < i; factor++) {
             while (i % factor == 0 && i != factor) {
@@ -17,5 +17,20 @@ public class PrimeFactor {
         }
         result.add(i);
         return result;
+    }
+
+    public static List factors(int n) {
+        return factors2(n, 2);
+    }
+
+    public static List factors2(int n, int p) {
+        List lst = new ArrayList();
+        if (n < 2) return lst;
+        if (n % p == 0) {
+            lst.add(p);
+            lst.addAll(factors2(n / p, p));
+            return lst;
+        }
+        return factors2(n, p + 1);
     }
 }
